d3b8762b62b4d3125a32d7fa433248de7bdb9b67
Harsh Vikram Singh <singh.harshvikram@gmail.com>
2020-09-10 10:50:57 +0530

    added the functionality to get all messages

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,12 @@
 var models = require('../models');
 
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
+  get: function (req, res) {
+    // read from req, what the user want
+    let tableName = req.route.path.slice(1);
+    models.messages.getAll(tableName, function (err, messages) {
+      res.send(messages);
+    });
+  }, // a function which handles a get request for all messages
   post: function (req, res) {} // a function which handles posting a message to the database
 };
--- server/models/messages.js
+++ server/models/messages.js
@@ -1,6 +1,16 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {}, // a function which produces all the messages
+  getAll: function (tableName, callback) {
+    // a function which produces all the messages
+    db.dbConnection.connect();
+    db.dbConnection.query('SELECT * FROM messages', [], function(err, results) {
+      if (err) {
+        console.log('getAll Function: something happened, but it is an error');
+      } else {
+        callback(null, results);
+      }
+    });
+  },
   create: function () {} // a function which can be used to insert a message into the database
 };
